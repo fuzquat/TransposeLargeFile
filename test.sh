ccda74fbf62f873c9273826a5a78b7168bdc9c24
fuzquat <fuzquat@>
2015-08-16 18:57:22 -0700

    Fix build/test

--- test.sh
+++ test.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-../Release/TransposeStuff.exe i $1 $1.intermediate
+./transpose i $1 $1.intermediate
 sort -n -k1,1 -k2,2 $1.intermediate -o $1.sorted
-../Release/TransposeStuff.exe m $1.sorted $1.transposed
+./transpose m $1.sorted $1.transposed
 
-../Release/TransposeStuff.exe i $1.transposed $1.intermediate2
+./transpose i $1.transposed $1.intermediate2
 sort -n -k1,1 -k2,2 $1.intermediate2 -o $1.sorted2
-../Release/TransposeStuff.exe m $1.sorted2 $1.recreated
+./transpose m $1.sorted2 $1.recreated
